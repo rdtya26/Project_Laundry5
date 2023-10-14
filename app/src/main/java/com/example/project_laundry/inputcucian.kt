f9837e3f85cad1836ad3c487bdc03ea1f73d304e
WahyuDwiKurniawan <tangut745@gmail.com>
2023-10-13 17:47:00 -0700

    inputcucian

--- app/src/main/java/com/example/project_laundry/inputcucian.kt
+++ app/src/main/java/com/example/project_laundry/inputcucian.kt
@@ -2,10 +2,40 @@ package com.example.project_laundry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_laundry.ROOM.Laundry
+import com.example.project_laundry.ROOM.database_laundry
+import com.example.project_laundry.databinding.ActivityInputcucianBinding
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class inputcucian : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInputcucianBinding
+    private val db by lazy { database_laundry(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inputcucian)
+        binding = ActivityInputcucianBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.inputcuci.setOnClickListener {
+            this.insertcucian()
+        }
+    }
+    private fun insertcucian(){
+        CoroutineScope(Dispatchers.IO).launch {
+            db.laundrydao().simpan(
+                Laundry(
+                    id = 0,
+                    binding.inputnamacuci.text.toString(),
+                    binding.inputnocuci.text.toString().toInt(),
+                    binding.inputberatcuci.text.toString(),
+                    binding.inputjeniscuci.text.toString(),
+                    binding.inputdetailcuci.text.toString(),
+                    binding.inputvariancuci.text.toString()
+                )
+            )
+        }
     }
 }
