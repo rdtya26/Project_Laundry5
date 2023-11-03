7e025c1beede5ca0b151c7e0e3907202ea2ba5e9
ZahwaLeafathraa <zahwaleafathra@gmail.com>
2023-11-03 10:16:39 +0700

    push

--- app/src/main/java/com/example/project_laundry/login.kt
+++ app/src/main/java/com/example/project_laundry/login.kt
@@ -6,11 +6,36 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-
 class login : AppCompatActivity() {
+
+    var et_user = findViewById<EditText>(R.id.hintuser)
+    var et_pass = findViewById<EditText>(R.id.hintpass)
+    var btnlogin = findViewById<Button>(R.id.login)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+            btnlogin.setOnClickListener {
+                var username = et_user
+                var password = et_pass
+                Toast.makeText(this@login, "username", Toast.LENGTH_SHORT)
+                    .show()
+
+                if (username.getText().toString().length == 0) {
+                    //jika username kosong
+                    username.setError("mohon isi username!!")
+                } else
+                    if (password.getText().toString().length == 0) {
+                        //jika password kosong
+                        password.setError("mohon isi password!!")
+                    }
+
+
+            }
+
+
+        }
     }
-}
+
